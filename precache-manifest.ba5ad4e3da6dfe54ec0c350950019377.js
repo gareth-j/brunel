48b6c69ba458fcde5205b487b9715a107797769f
chryswoods <chryswoods@gmail.com>
2020-01-17 10:49:39 +0000

    Updates

---
+++ precache-manifest.ba5ad4e3da6dfe54ec0c350950019377.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab732314ed6ce95f776c5ae528664b7",
+    "url": "/brunel/index.html"
+  },
+  {
+    "revision": "a84a00b9e043df576515",
+    "url": "/brunel/static/css/2.532ce858.chunk.css"
+  },
+  {
+    "revision": "fe3174115bdb9926589d",
+    "url": "/brunel/static/css/3.a023d884.chunk.css"
+  },
+  {
+    "revision": "616e8487612a1e3f4b51",
+    "url": "/brunel/static/css/4.3027a02b.chunk.css"
+  },
+  {
+    "revision": "18ccc1eaa87a69bcd8ad",
+    "url": "/brunel/static/css/main.7040ee57.chunk.css"
+  },
+  {
+    "revision": "a84a00b9e043df576515",
+    "url": "/brunel/static/js/2.fceddb58.chunk.js"
+  },
+  {
+    "revision": "321bb6bbd6cec3df09f5d5eb3bd7cbb2",
+    "url": "/brunel/static/js/2.fceddb58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe3174115bdb9926589d",
+    "url": "/brunel/static/js/3.51cd0943.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/brunel/static/js/3.51cd0943.chunk.js.LICENSE"
+  },
+  {
+    "revision": "616e8487612a1e3f4b51",
+    "url": "/brunel/static/js/4.3ec60954.chunk.js"
+  },
+  {
+    "revision": "18ccc1eaa87a69bcd8ad",
+    "url": "/brunel/static/js/main.d228789c.chunk.js"
+  },
+  {
+    "revision": "6331208253f0c421499d",
+    "url": "/brunel/static/js/runtime-main.f1d7ee07.js"
+  },
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/brunel/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/brunel/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/brunel/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/brunel/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/brunel/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/brunel/static/media/editIcon.d5422321.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/brunel/precache-manifest.89ad726823d44958a20b49e91c6561d5.js"
+  "/brunel/precache-manifest.ba5ad4e3da6dfe54ec0c350950019377.js"
 );
 
 self.addEventListener('message', (event) => {
